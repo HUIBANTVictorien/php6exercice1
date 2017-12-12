04d5927b4dfb6bdc0caabcdb4b211cc1b55f9646
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 13:45:00 +0100

    ajout exo1

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<?php
-$number = 0;
-while ($number <= 10) {
-  echo $number.'<br/>';
-  $number ++;
-}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 1 partie 4 php</title>
+  </head>
+  <body>
+    <?php
+    function returnTrue() {
+      return true;
+    }
+    echo returnTrue();
+    ?>
+  </body>
+</html>
